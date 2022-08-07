149fc23c65090d49dfa7ac882c322ce01d3c1649
Tatsuki Nakamori <sss.53.88840@gmail.com>
2022-08-07 09:42:05 +0900

    時刻の追加

--- api/root.py
+++ api/root.py
@@ -3,11 +3,13 @@
 """
 
 from flask import Blueprint, jsonify, make_response
+from datetime import datetime
 
 app = Blueprint("root", __name__)
 
 @app.route("/")
 def root():
     return make_response(jsonify({
-        "message": "MARINE TRASHART"
+        "message": "MARINE TRASHART",
+        "time": datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
     }), 200)
