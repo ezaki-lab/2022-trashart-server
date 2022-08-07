d51756e6712194cb82d7e4ef1a55e631e4c09274
takara2314 <takara2314_TK@outlook.jp>
2022-08-07 09:48:45 +0900

    Remove: テスト文を削除

--- api/root.py
+++ api/root.py
@@ -3,13 +3,11 @@
 """
 
 from flask import Blueprint, jsonify, make_response
-from datetime import datetime
 
 app = Blueprint("root", __name__)
 
 @app.route("/")
 def root():
     return make_response(jsonify({
-        "message": "MARINE TRASHART",
-        "time": datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
+        "message": "MARINE TRASHART"
     }), 200)
