9b10ed54475b6ef66e3f6ba5a784dea5e5d9b3ad
takara2314 <takara2314_TK@outlook.jp>
2022-09-07 14:13:08 +0900

    Add: Base64をそのまま保存するAPIを作成

--- api/pick.py
+++ api/pick.py
@@ -5,13 +5,30 @@
 from flask import Blueprint, Flask, jsonify, make_response
 from flask_restful import Api, abort, Resource
 
+from datetime import datetime
 from logger import logger
 from services.inspector import content_type
 import services.crafting.api as service
 from services.pick.store.post import MaterialSeparator
+from utils.base64_to_file import Base64_to_file
 
 app = Blueprint("pick", __name__)
 api = Api(app, errors=Flask.errorhandler)
+
+class PickSeparate(Resource):
+    @logger
+    @content_type("application/json")
+    def post(self):
+        args = service.store_parser.parse_args()
+
+        try:
+            converter = Base64_to_file(args["data"])
+            basename = datetime.now().strftime("%H-%M-%S")
+            path = converter.save("storage/chousa/", basename+".png")
+        except Exception as e:
+            abort(400)
+
+        return make_response(jsonify({}), 200)
 
 class PickStore(Resource):
     @logger
